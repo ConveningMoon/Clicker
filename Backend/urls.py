1dafc500043460b94261d9f2b5796d51425b72d7
ConveningMoon <dj.vergara@hotmail.com>
2022-06-01 09:18:07 +0500

    added backend and frontend

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
 
+boosts = views.BoostViewSet.as_view({   
+    'get': 'list',
+    'post': 'create',
+})
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
+    path('call_click/', views.call_click),
+    path('logout/', views.user_logout, name='logout'),
+    path('', views.index, name='index'),
+    path('boosts/', boosts, name='boosts'),
 ]
--- Backend/views.py
+++ Backend/views.py
@@ -2,35 +2,78 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm
+from .models import Core, Boost
+
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from django.http import HttpResponse
+from .serializers import CoreSerializer, BoostSerializer
 
 
-def register(request):  
-    if request.method == 'POST':  
-        form = UserForm(request.POST)  
-        if form.is_valid():  
-            user = form.save()  
-            login(request, user)  
-            return redirect('index') 
-
-        return render(request, 'register.html', {'form': form})  
-      
-    form = UserForm()  
-    return render(request, 'register.html', {'form': form})
-
-def user_login(request):  
-    form = UserForm() 
+@login_required
+def index(request): 
+    core = Core.objects.get(user=request.user) 
+    boosts = Boost.objects.filter(core=core) # Достаем бусты пользователя из базы 
      
-    if request.method == 'POST':  
-        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))  
-        if user: 
-            login(request, user)  
-            return redirect('index')
+    return render(request, 'index.html', { 
+        'core': core, 
+        'boosts': boosts, # Возвращаем бусты на фронтик
+    })
 
-        return render(request, 'login.html', {'form': form, 'invalid': True})
-
-    return render(request, 'login.html', {'form': form})
 
 @login_required
 def user_logout(request): 
     logout(request) 
     return redirect('login')
+
+class Register(APIView): 
+    def get(self, request): 
+        form = UserForm() 
+        return render(request, 'register.html', {'form': form}) 
+    def post(self, request): 
+        form = UserForm(request.POST)   
+        if form.is_valid():   
+            user = form.save()   
+            login(request, user)
+
+            core = Core(user=user) 
+            core.save() 
+            return redirect('index') 
+
+        return render(request, 'register.html', {'form': form})
+
+class Login(APIView): 
+    form = UserForm() 
+    def get(self, request): 
+        return render(request, 'login.html', {'form': self.form})
+
+    def post(self, request): 
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))  
+        if user: 
+            login(request, user)  
+            return redirect('index') 
+        return render(request, 'login.html', {'form': self.form, 'invalid': True})
+
+@api_view(['GET']) 
+@login_required 
+def call_click(request): 
+    core = Core.objects.get(user=request.user) 
+    is_levelup = core.click() # Труе если буст создался
+    if is_levelup: 
+        Boost.objects.create(core=core, price=core.level*50, power=core.level*20) # Создание буста  
+    core.save()
+
+    return Response({ 'core': CoreSerializer(core).data, 'is_levelup': is_levelup })
+
+class BoostViewSet(viewsets.ModelViewSet):  
+    queryset = Boost.objects.all()  
+    serializer_class = BoostSerializer
+
+    def get_queryset(self): 
+        core = Core.objects.get(user=self.request.user) 
+        boosts = Boost.objects.filter(core=core)
+        return boosts
